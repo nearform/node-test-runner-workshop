a00615daf307ddccd9b8e647a6b8cf73eab11b7e
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-02-13 11:34:35 +0100

    Refactpr

--- src/a02-assertions/verifiers/handler.js
+++ src/a02-assertions/verifiers/handler.js
@@ -7,30 +7,38 @@ await import('../test/index.test.js')
 // I'm creating this wrapper in order to be able to spy the process.exit
 process.on('exit', () => {
   console.log('Validating the test completion...')
-  if (test.mock.calls.length < 2)
-    throw new Error(
-      'You need to create a test for both the sum and sumAsync functions'
-    )
 
-  if (
-    typeof assertCalls.find(
-      a => a.testName === 'sum' && a.method === 'deepStrictEqual'
-    ) === 'undefined'
-  )
-    throw new Error('You need to call "deepStrictEqual" inside the "sum" test')
+  const expectations = [
+    {
+      condition: test.mock.calls.length < 2,
+      message:
+        'You need to create a test for both the sum and sumAsync functions'
+    },
+    {
+      condition:
+        typeof assertCalls.find(
+          a => a.testName === 'sum' && a.method === 'deepStrictEqual'
+        ) === 'undefined',
+      message: 'You need to call "deepStrictEqual" inside the "sum" test'
+    },
+    {
+      condition:
+        typeof assertCalls.find(
+          a => a.testName === 'sumAsync' && a.method === 'deepStrictEqual'
+        ) === 'undefined',
+      message: 'You need to call "deepStrictEqual" inside the "sumAsync" test'
+    },
+    {
+      condition: sum.mock.calls.length < 1,
+      message: 'You need to call "sum" at least once in your test'
+    },
+    {
+      condition: sumAsync.mock.calls.length < 1,
+      message: 'You need to call "sumAsync" at least once in your test'
+    }
+  ]
 
-  if (
-    typeof assertCalls.find(
-      a => a.testName === 'sumAsync' && a.method === 'deepStrictEqual'
-    ) === 'undefined'
-  )
-    throw new Error(
-      'You need to call "deepStrictEqual" inside the "sumAsync" test'
-    )
-
-  if (sum.mock.calls.length < 1)
-    throw new Error('You need to call "sum" at least once in your test')
-
-  if (sumAsync.mock.calls.length < 1)
-    throw new Error('You need to call "sumAsync" at least once in your test')
+  for (const expectation of expectations) {
+    if (expectation.condition) throw new Error(expectation.message)
+  }
 })
