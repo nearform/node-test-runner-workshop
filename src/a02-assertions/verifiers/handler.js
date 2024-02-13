46abbcca93f22db8e69e6ad382decac9a70442e7
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-02-13 11:30:51 +0100

    Wording fix

--- src/a02-assertions/verifiers/handler.js
+++ src/a02-assertions/verifiers/handler.js
@@ -6,7 +6,7 @@ await import('../test/index.test.js')
 
 // I'm creating this wrapper in order to be able to spy the process.exit
 process.on('exit', () => {
-  console.log('Validating the test completeness...')
+  console.log('Validating the test completion...')
   if (test.mock.calls.length < 2)
     throw new Error(
       'You need to create a test for both the sum and sumAsync functions'
