4d25bdf8105bcf45b377af1daa93c42d8ac344c2
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-02-13 12:23:33 +0100

    A04 hooks verifiers

--- verify/test.verify.js
+++ verify/test.verify.js
@@ -1,5 +1,12 @@
 import { AsyncLocalStorage } from 'async_hooks'
-import { test as originalTest, mock } from 'node:test'
+import {
+  test as originalTest,
+  before as originalBefore,
+  beforeEach as originalBeforeEach,
+  afterEach as originalAfterEach,
+  after as originalAfter,
+  mock
+} from 'node:test'
 
 const asyncLocalStorage = new AsyncLocalStorage()
 
@@ -8,8 +15,36 @@ export const test = mock.fn((...args) => {
     return originalTest(...args)
   })
 })
-
 export function getCurrentTestName() {
   const store = asyncLocalStorage.getStore()
   return store ? store.get('testName') : undefined
+}
+
+export const before = mock.fn((...args) => {
+  asyncLocalStorage.run(new Map([['hook', 'before']]), () => {
+    return originalBefore(...args)
+  })
+})
+
+export const beforeEach = mock.fn((...args) => {
+  asyncLocalStorage.run(new Map([['hook', 'beforeEach']]), () => {
+    return originalBeforeEach(...args)
+  })
+})
+
+export const after = mock.fn((...args) => {
+  asyncLocalStorage.run(new Map([['hook', 'after']]), () => {
+    return originalAfter(...args)
+  })
+})
+
+export const afterEach = mock.fn((...args) => {
+  asyncLocalStorage.run(new Map([['hook', 'afterEach']]), () => {
+    return originalAfterEach(...args)
+  })
+})
+
+export function getHook() {
+  const store = asyncLocalStorage.getStore()
+  return store ? store.get('hook') : undefined
 }
