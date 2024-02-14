3d8fd442f01a7013a8a731cd10897c920f40b279
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-02-14 10:38:12 +0100

    Loader refactor

--- src/a02-assertions/verifiers/index.verify.js
+++
@@ -1,8 +0,0 @@
-import {
-  sum as originalSum,
-  sumAsync as originalSumAsync
-} from '../src/index.js'
-import { mock } from 'node:test'
-
-export const sum = mock.fn(originalSum)
-export const sumAsync = mock.fn(originalSumAsync)
