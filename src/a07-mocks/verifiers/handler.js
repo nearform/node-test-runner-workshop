f1247fad7a1f04b1361aa25831a8570145aebfe2
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-02-22 12:28:35 +0100

    Refactor verify entry point (#15)
    * fix(refactor): a02 refactoring
    
    * fix(refactor): a04
    
    * fix(refactor): a05
    
    * fix(refactor): a06
    
    * fix(refactor): a07
    
    * fix(refactor): a08

--- src/a07-mocks/verifiers/handler.js
+++ src/a07-mocks/verifiers/handler.js
@@ -1,19 +1,11 @@
-import { mockCalls } from '../../../verify/test.verify.js'
+import handler from '../../../verify/handler.js'
+import '../test/index.test.js'
 
-await import('../test/index.test.js')
-
-// I'm creating this wrapper in order to be able to spy the process.exit
-process.on('exit', () => {
-  console.log('Validating the test completion...')
-
-  const expectations = [
-    {
-      condition: !mockCalls.includes('fn'),
-      message: 'You need to call "mock.fn" at least once'
-    }
-  ]
-
-  for (const expectation of expectations) {
-    if (expectation.condition) throw new Error(expectation.message)
+const expectations = [
+  {
+    condition: ({ mockCalls }) => !mockCalls.includes('fn'),
+    message: 'You need to call "mock.fn" at least once'
   }
-})
+]
+
+handler(expectations)
