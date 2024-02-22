ce53b1a4a1002aceedcdba7d2ec163d8685fb317
Alfonso Graziano <alfonso.graziano@nearform.com>
2024-02-22 12:31:56 +0100

    fix(refactor): a09 handler refactor

--- src/a09-dates/verifiers/handler.js
+++ src/a09-dates/verifiers/handler.js
@@ -1,35 +1,25 @@
 import { getCurrentFormattedDate } from './index.verify.js'
-import { mockCalls } from '../../../verify/test.verify.js'
-import { assertCalls } from '../../../verify/assert.verify.js'
+import handler from '../../../verify/handler.js'
+import '../test/index.test.js'
 
-await import('../test/index.test.js')
-
-// I'm creating this wrapper in order to be able to spy the process.exit
-process.on('exit', () => {
-  console.log('Validating the test completion...')
-
-  const expectations = [
-    {
-      condition: !mockCalls.includes('timers.enable'),
-      message: 'You need to call "timers.enable" at least once'
-    },
-    {
-      condition: !mockCalls.includes('timers.setTime'),
-      message: 'You need to call "timers.setTime" at least once'
-    },
-    {
-      condition: getCurrentFormattedDate.mock.calls.length === 0,
-      message: 'You need to call "getCurrentFormattedDate" at least once'
-    },
-    {
-      condition:
-        typeof assertCalls.find(a => a.method === 'strictEqual') ===
-        'undefined',
-      message: 'You need to call "strictEqual" inside the test'
-    }
-  ]
-
-  for (const expectation of expectations) {
-    if (expectation.condition) throw new Error(expectation.message)
+const expectations = [
+  {
+    condition: ({ mockCalls }) => !mockCalls.includes('timers.enable'),
+    message: 'You need to call "timers.enable" at least once'
+  },
+  {
+    condition: ({ mockCalls }) => !mockCalls.includes('timers.setTime'),
+    message: 'You need to call "timers.setTime" at least once'
+  },
+  {
+    condition: () => getCurrentFormattedDate.mock.calls.length === 0,
+    message: 'You need to call "getCurrentFormattedDate" at least once'
+  },
+  {
+    condition: ({ assertCalls }) =>
+      typeof assertCalls.find(a => a.method === 'strictEqual') === 'undefined',
+    message: 'You need to call "strictEqual" inside the test'
   }
-})
+]
+
+handler(expectations)
