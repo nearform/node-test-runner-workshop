cbcb0a66d5e2133d20aef168b3887066f27785f0
Mohammed Bilal Shareef <bilalshareef.h@gmail.com>
2024-03-25 17:26:06 +0530

    fix: update variable name

--- src/a03-parallel-testing/test/multiply2.test.js
+++ src/a03-parallel-testing/test/multiply2.test.js
@@ -4,17 +4,17 @@ import { heavyComputationMultiply } from '../src/index.js'
 
 test('test some heavy computation multiply functions', async () => {
   await test('2 * 100_000_000', async () => {
-    const multiply = await heavyComputationMultiply(2, 100_000_000)
-    assert.deepStrictEqual(multiply, 200_000_000)
+    const product = await heavyComputationMultiply(2, 100_000_000)
+    assert.deepStrictEqual(product, 200_000_000)
   })
 
   await test('4 * 100_000_000', async () => {
-    const multiply = await heavyComputationMultiply(4, 100_000_000)
-    assert.deepStrictEqual(multiply, 400_000_000)
+    const product = await heavyComputationMultiply(4, 100_000_000)
+    assert.deepStrictEqual(product, 400_000_000)
   })
 
   await test('1000 * 100_000_000', async () => {
-    const multiply = await heavyComputationMultiply(1000, 100_000_000)
-    assert.deepStrictEqual(multiply, 100_000_000_000)
+    const product = await heavyComputationMultiply(1000, 100_000_000)
+    assert.deepStrictEqual(product, 100_000_000_000)
   })
 })
