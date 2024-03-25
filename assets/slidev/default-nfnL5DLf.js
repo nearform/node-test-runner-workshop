3d9ee61a54e1d7d2f5d617c3d997d43020c290c0
simoneb <simoneb@users.noreply.github.com>
2024-03-25 12:03:01 +0000

    deploy: a0cea6d3b53ea7b609cabb9f4440d5ff860cd341

---
+++ assets/slidev/default-nfnL5DLf.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as r}from"../modules/vue-CaZp_oFZ.js";import{b as s}from"../index-nkNYhXHm.js";const n={},l={class:"slidev-layout default"};function a(e,c){return o(),t("div",l,[r(e.$slots,"default")])}const _=s(n,[["render",a],["__file","/home/runner/work/node-test-runner-workshop/node-test-runner-workshop/node_modules/@slidev/client/layouts/default.vue"]]);export{_ as I};
