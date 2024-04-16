23eaead4feca770b6a6f191561a7ff183e2ad03f
Jose Ortiz <9669217+sudopriestmx@users.noreply.github.com>
2024-04-16 10:56:48 -0600

    Fix: use getUTCDate to align across timezones and not fail test (#42)
    Co-authored-by: Jose Pablo Ortiz Lack <josepablo.ortiz@naranja.com>

--- src/a09-dates/src/index.js
+++ src/a09-dates/src/index.js
@@ -2,5 +2,5 @@ export function getCurrentFormattedDate() {
   const now = new Date()
   return `${now.getFullYear()}-${(now.getMonth() + 1)
     .toString()
-    .padStart(2, '0')}-${now.getDate().toString().padStart(2, '0')}`
+    .padStart(2, '0')}-${now.getUTCDate().toString().padStart(2, '0')}`
 }
